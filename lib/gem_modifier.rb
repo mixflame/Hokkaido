09f5f8fd0953d7298bd4a23550f413dfe8a6d852
Jonathan "jsilver1er" Silverman <jsilverone@me.com>
2012-10-11 13:30:01 -0700

    support autoload folding #2

--- lib/gem_modifier.rb
+++ lib/gem_modifier.rb
@@ -2,6 +2,8 @@ require 'ruby_parser'
 require 'fileutils'
 require 'tempfile'
 
+require 'pry'
+
 # require removal only
 
 class File
@@ -82,6 +84,33 @@ module Hokkaido
           # comment it out
           current_file += "# #{line}"
           next
+        elsif line.strip =~ /^autoload/
+          # same as require
+          parser = RubyParser.new
+          sexp = parser.parse(line)
+          call = sexp[2]
+
+          unless call == :autoload
+            # WEIRD SHIT IS HAPPENING
+            current_file += line
+            next
+          end
+
+          require_type = sexp[3][2][0]
+          library = sexp[3][2][1]
+
+          full_rb_path = File.join([@lib_folder, "#{library}.rb"])
+          unless @require_libs.include?(full_rb_path)
+            file_index = @require_libs.index(init_lib)
+            insert_index = file_index
+            @require_libs.insert insert_index, full_rb_path
+            parse_gem(full_rb_path)
+          end
+          # comment it out
+          current_file += "# #{line}"
+          next
+
+          #binding.pry
         elsif line.strip =~ /^eval/
           # comment it out
           current_file += "# FIXME: #eval is not available in motion\n"
